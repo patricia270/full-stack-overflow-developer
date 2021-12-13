a80b0ae8698a2418ea0fc297800f69e1a9991596
patricia270 <patriciasilva.eq@gmail.com>
2021-12-13 11:52:06 -0400

    fix: Correction of the method when updating scores

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import * as questionController from './controllers/questionController';
-import * as userController from './controllers/userController';
-import * as answerController from './controllers/answerController';
+import router from './routers/questionRouter';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/questions', questionController.postQuestion);
-app.get('/questions', questionController.getQuestions);
-
-app.get('/questions/:id', questionController.getQuestionById);
-app.post('/questions/:id', answerController.postAnswer);
-
-app.post('/users', userController.registerUser);
-app.get('/ranking', userController.getRanking);
-
-app.post('/questions/:id/up-vote', questionController.postUpvote);
-app.post('/questions/:id/down-vote', questionController.postDownvote);
+app.use(router);
 
 export default app;
